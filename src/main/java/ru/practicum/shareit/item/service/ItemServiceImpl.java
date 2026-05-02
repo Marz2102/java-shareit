1c823e49daa88d44eb527750f99450a2c0e23bdb
Marz2102 <kostomarov2001@yandex.ru>
2026-05-03 01:19:49 +0300

    Add item and user repository

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -7,7 +7,7 @@ import ru.practicum.shareit.item.ItemMapper;
 import ru.practicum.shareit.item.dto.ItemCreateDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.storage.ItemStorage;
+import ru.practicum.shareit.item.storage.ItemRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
@@ -15,24 +15,24 @@ import java.util.List;
 
 @Service
 public class ItemServiceImpl implements ItemService {
-    private final ItemStorage itemStorage;
+    private final ItemRepository itemRepository;
     private final UserService userService;
 
-    public ItemServiceImpl(final ItemStorage itemStorage, final UserService userService) {
-        this.itemStorage = itemStorage;
+    public ItemServiceImpl(final ItemRepository itemRepository, final UserService userService) {
+        this.itemRepository = itemRepository;
         this.userService = userService;
     }
 
     @Override
     public ItemDto getItemById(Long id, Long userId) {
-        return ItemMapper.toItemDto(itemStorage.findById(id)
+        return ItemMapper.toItemDto(itemRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Предмет с id - " + id + " не найден")));
     }
 
     @Override
     public List<ItemDto> getItems(Long userId) {
-        return itemStorage
-                .getItems(userId)
+        return itemRepository
+                .findByOwnerId(userId)
                 .stream()
                 .map(ItemMapper::toItemDto)
                 .toList();
@@ -45,19 +45,19 @@ public class ItemServiceImpl implements ItemService {
 
         item.setOwner(user);
 
-        return ItemMapper.toItemDto(itemStorage.addItem(item));
+        return ItemMapper.toItemDto(itemRepository.save(item));
     }
 
     @Override
     public ItemDto updateItem(Long id, ItemCreateDto itemCreateDto, Long userId) {
-        Item item = itemStorage.findById(id)
+        Item item = itemRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Предмет с id - " + id + " не найден"));
 
         if (item.getOwner() != null && !item.getOwner().getId().equals(userId)) {
             throw new DataAccessException("У пользователя с id - " + userId + " нет возможности редактировать предмет с id - " + item.getId());
         }
 
-        return ItemMapper.toItemDto(itemStorage.updateItem(ItemMapper.updateItem(item, itemCreateDto)));
+        return ItemMapper.toItemDto(itemRepository.save(ItemMapper.updateItem(item, itemCreateDto)));
     }
 
     @Override
@@ -66,7 +66,7 @@ public class ItemServiceImpl implements ItemService {
             return List.of();
         }
 
-        return itemStorage
+        return itemRepository
                 .searchItems(query.toLowerCase())
                 .stream()
                 .map(ItemMapper::toItemDto)
